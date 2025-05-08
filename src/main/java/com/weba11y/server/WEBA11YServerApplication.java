111c38c644ca08e81c8ebde5c72a8fd23396af24
Seongmo Gu <gusm96@naver.com>
2025-05-08 14:16:46 +0900

    fix : build.gradle & application.yml 수정

--- src/main/java/com/weba11y/server/WEBA11YServerApplication.java
+++ src/main/java/com/weba11y/server/WEBA11YServerApplication.java
@@ -1,6 +1,5 @@
 package com.weba11y.server;
 
-import com.weba11y.server.configuration.EnvLoader;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
@@ -14,8 +13,6 @@ import org.springframework.data.r2dbc.repository.config.EnableR2dbcRepositories;
 public class WEBA11YServerApplication {
 
 	public static void main(String[] args) {
-		// Spring Boot 실행 전에 .env 파일을 환경 변수로 로드
-		EnvLoader.loadEnv("src/main/resources/.env");
 		SpringApplication.run(WEBA11YServerApplication.class, args);
 	}
 
