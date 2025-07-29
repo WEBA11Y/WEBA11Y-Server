2e33be2407fcb3e4c8ba1b435e0650f23d269938
Seongmo Gu <gusm96@naver.com>
2025-07-29 17:32:24 +0900

    feat : OperableControlsCheck 검사 구현체 추가

---
+++ src/main/java/com/weba11y/server/checker/implement/OperableControlsCheck.java
@@ -0,0 +1,46 @@
+package com.weba11y.server.checker.implement;
+
+import com.microsoft.playwright.ElementHandle;
+import com.microsoft.playwright.Page;
+import com.weba11y.server.checker.AbstractAccessibilityChecker;
+import com.weba11y.server.checker.DynamicChecker;
+import com.weba11y.server.domain.enums.InspectionItems;
+import com.weba11y.server.dto.accessibilityViolation.AccessibilityViolationDto;
+import lombok.extern.slf4j.Slf4j;
+import org.jsoup.nodes.Element;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Slf4j
+@Component
+public class OperableControlsCheck extends AbstractAccessibilityChecker implements DynamicChecker {
+
+    @Override
+    protected InspectionItems getItem() {
+        return InspectionItems.OPERABLE_CONTROLS;
+    }
+
+    @Override
+    public List<AccessibilityViolationDto> checkDynamic(Page page, Long summaryId) {
+        List<AccessibilityViolationDto> violations = new ArrayList<>();
+
+        List<ElementHandle> controls = page.querySelectorAll("a, button, input[type=button], input[type=submit]");
+        for (ElementHandle el : controls) {
+            int width = Integer.parseInt(page.evaluate("el => el.offsetWidth", el).toString());
+            int height = Integer.parseInt(page.evaluate("el => el.offsetHeight", el).toString());
+
+            if (width < 44 || height < 44) {
+                violations.add(AccessibilityViolationDto.createViolationDto(el, getItem(), summaryId));
+                log.debug("[OperableControlsCheck] 조작 영역 부족: {}x{}, element={}", width, height, el);
+            }
+        }
+        return violations;
+    }
+
+    @Override
+    protected boolean isViolation(Element element) {
+        return false;
+    }
+}
