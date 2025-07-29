bc1112e6f6e70fdf6d89687853331a5bb3e7881c
Seongmo Gu <gusm96@naver.com>
2025-07-29 17:30:12 +0900

    refactor : AltTextCheck 리팩터링

--- src/main/java/com/weba11y/server/checker/implement/AltTextCheck.java
+++ src/main/java/com/weba11y/server/checker/implement/AltTextCheck.java
@@ -1,36 +1,21 @@
 package com.weba11y.server.checker.implement;
 
-import com.weba11y.server.checker.AccessibilityChecker;
-import com.weba11y.server.dto.accessibilityViolation.AccessibilityViolationDto;
-import lombok.extern.slf4j.Slf4j;
-import org.jsoup.nodes.Document;
+import com.weba11y.server.checker.AbstractAccessibilityChecker;
+import com.weba11y.server.domain.enums.InspectionItems;
+import org.jsoup.nodes.Element;
 import org.springframework.stereotype.Component;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.stream.Collectors;
-
-import static com.weba11y.server.domain.enums.InspectionItems.*;
-
-@Slf4j
 @Component
-public class AltTextCheck implements AccessibilityChecker {
+public class AltTextCheck extends AbstractAccessibilityChecker {
 
     @Override
-    public List<AccessibilityViolationDto> check(Document doc, Long inspectionSummaryId) {
-        List<AccessibilityViolationDto> violations = new ArrayList<>();
+    protected InspectionItems getItem() {
+        return InspectionItems.ALT_TEXT;
+    }
 
-        for (String target : ALT_TEXT.getTargetTags()) {
-            List<AccessibilityViolationDto> tagViolations = doc.select(target)
-                    .stream()
-                    .filter(element -> !element.hasAttr("alt") || element.attr("alt").isEmpty())
-                    .map(element -> {
-                        log.debug("[AltTextCheck] ALT 누락 요소 발견: <{}>", element.tagName());
-                        return AccessibilityViolationDto.createInspectionResultDto(element, ALT_TEXT, inspectionSummaryId);
-                    }).collect(Collectors.toList());
-            violations.addAll(tagViolations);
-        }
-        return violations;
+    @Override
+    protected boolean isViolation(Element element) {
+        return !element.hasAttr("alt") || element.attr("alt").isBlank();
     }
 }
 
